3e436677a64c3b359020b230812cce368dd0e2fd
Mr. Tiny Sudo Meathead <omiteoin@gmail.com>
2025-11-29 09:10:49 +0800

    add light and dark mode (#5)

---
+++ app/components/app/ThemeToggle.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(value) {
+    colorMode.preference = value ? "dark" : "light";
+  },
+});
+</script>
+
+<template>
+  <label class="swap swap-rotate">
+    <!-- this hidden checkbox controls the state -->
+    <input v-model="isDark" type="checkbox">
+
+    <!-- sun icon -->
+    <Icon name="tabler:sunset-2-filled" size="24" class="swap-on" />
+
+    <!-- moon icon -->
+    <Icon name="tabler:haze-moon" size="24" class="swap-off" />
+  </label>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode"],
   css: ["./app/assets/css/main.css"],
   vite: {
     plugins: [
@@ -15,5 +15,8 @@ export default defineNuxtConfig({
     config: {
       standalone: false,
     },
+  },
+  colorMode: {
+    dataValue: "theme",
   },
 });
