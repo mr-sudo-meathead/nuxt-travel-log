c954de1629dd1963bc1f40065c999b52b588d479
mr-sudo-meathead <omiteoin@gmail.com>
2025-11-29 09:09:36 +0800

    add light and dark mode

--- app/components/app/NavBar.vue
+++ app/components/app/NavBar.vue
@@ -5,7 +5,8 @@
         Travel Log
       </NuxtLink>
     </div>
-    <div class="navbar-end">
+    <div class="navbar-end gap-4">
+      <AppThemeToggle />
       <a class="btn bg-green-500">
         Sign In With Github
         <Icon name="tabler:brand-github" size="24" />
