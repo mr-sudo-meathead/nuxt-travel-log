1af15f532d728e8c55d3f688a8b801f567a9cd09
mr-sudo-meathead <omiteoin@gmail.com>
2025-12-01 01:55:06 +0800

    add auth state / sign out

--- app/components/AuthButton.vue
+++ app/components/AuthButton.vue
@@ -5,7 +5,23 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <button :disabled="authStore.loading" class="btn bg-green-500" @click="authStore.signIn">
+  <details v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+    <summary class="btn m-1">
+      {{ authStore.user.name }}
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-8 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+    </summary>
+    <ul class="menu dropdown-content bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+      <NuxtLink to="/sign-out" class="flex flex-row items-center gap-2 hover:bg-gray-50/5 hover:rounded">
+        <a>Sign out</a>
+        <Icon name="tabler:door-exit" />
+      </NuxtLink>
+    </ul>
+  </details>
+  <button v-else :disabled="authStore.loading" class="btn bg-green-500" @click="authStore.signIn">
     Sign In With Github
     <span v-if="authStore.loading" class="loading loading-spinner text-green-300" />
     <Icon v-else name="tabler:brand-github" size="24" />
